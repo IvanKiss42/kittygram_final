318d5e4580e290b2ef41aeeaff3e3f92abfcf9c2
Иван Кисляков <hodgivan@gmail.com>
2024-03-21 21:10:21 +0500

    For portrolio

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.5.135', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']     # add your server IP here
 
 INSTALLED_APPS = [
     'django.contrib.admin',
